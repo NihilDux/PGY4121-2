8e4211a67463859c0a0c2f4488009cb6f49962ee
NihilDux <ang.curin@duocuc.cl>
2023-10-13 21:07:51 -0300

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private http: HttpClient ) {}
 
 }
